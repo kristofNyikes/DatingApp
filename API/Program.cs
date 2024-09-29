5a80931b477d89d59a572d6586a2751f19191c49
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-09-29 18:57:19 +0200

    feat: create skeleton angular app with cors

--- API/Program.cs
+++ API/Program.cs
@@ -16,10 +16,14 @@ namespace API
             {
                 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddCors();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+
             app.MapControllers();
 
             app.Run();
